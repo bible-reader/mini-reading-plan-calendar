ab3289c619d5ff3108e4730724c990306e6bc982
Pavel Struhar <pstruhar@gmail.com>
2018-01-22 23:13:39 +0100

    NZ->NT, SZ->OT

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -12,54 +12,54 @@ export default class Calendar extends Component {
     const nameNT = `${year} - New Testament`;
     const nameOT = `${year} - Old Testament`;
 
-    const dataNZ = getData(passagesNT);
-    const dataSZ = getData(passagesOT);
+    const dataNT = getData(passagesNT);
+    const dataOT = getData(passagesOT);
 
     return (
       <div>
         <div className="left page outlined">
           <h2>{nameNT} - 1. polrok</h2>
           <div className="months">
-            <Month monthNumber={1} monthData={dataNZ[1]} year={year} />
-            <Month monthNumber={2} monthData={dataNZ[2]} year={year} />
-            <Month monthNumber={3} monthData={dataNZ[3]} year={year} />
-            <Month monthNumber={4} monthData={dataNZ[4]} year={year} />
-            <Month monthNumber={5} monthData={dataNZ[5]} year={year} />
-            <Month monthNumber={6} monthData={dataNZ[6]} year={year} />
+            <Month monthNumber={1} monthData={dataNT[1]} year={year} />
+            <Month monthNumber={2} monthData={dataNT[2]} year={year} />
+            <Month monthNumber={3} monthData={dataNT[3]} year={year} />
+            <Month monthNumber={4} monthData={dataNT[4]} year={year} />
+            <Month monthNumber={5} monthData={dataNT[5]} year={year} />
+            <Month monthNumber={6} monthData={dataNT[6]} year={year} />
           </div>
         </div>
         <div className="right page outlined page-break">
           <h2>{nameOT} - 1. polrok</h2>
           <div className="months">
-            <Month monthNumber={1} monthData={dataSZ[1]} year={year} />
-            <Month monthNumber={2} monthData={dataSZ[2]} year={year} />
-            <Month monthNumber={3} monthData={dataSZ[3]} year={year} />
-            <Month monthNumber={4} monthData={dataSZ[4]} year={year} />
-            <Month monthNumber={5} monthData={dataSZ[5]} year={year} />
-            <Month monthNumber={6} monthData={dataSZ[6]} year={year} />
+            <Month monthNumber={1} monthData={dataOT[1]} year={year} />
+            <Month monthNumber={2} monthData={dataOT[2]} year={year} />
+            <Month monthNumber={3} monthData={dataOT[3]} year={year} />
+            <Month monthNumber={4} monthData={dataOT[4]} year={year} />
+            <Month monthNumber={5} monthData={dataOT[5]} year={year} />
+            <Month monthNumber={6} monthData={dataOT[6]} year={year} />
           </div>
         </div>
         <div className="clr" />
         <div className="left page">
           <h2>{nameOT} - 2. polrok</h2>
           <div className="months">
-            <Month monthNumber={7} monthData={dataSZ[7]} year={year} />
-            <Month monthNumber={8} monthData={dataSZ[8]} year={year} />
-            <Month monthNumber={9} monthData={dataSZ[9]} year={year} />
-            <Month monthNumber={10} monthData={dataSZ[10]} year={year} />
-            <Month monthNumber={11} monthData={dataSZ[11]} year={year} />
-            <Month monthNumber={12} monthData={dataSZ[12]} year={year} />
+            <Month monthNumber={7} monthData={dataOT[7]} year={year} />
+            <Month monthNumber={8} monthData={dataOT[8]} year={year} />
+            <Month monthNumber={9} monthData={dataOT[9]} year={year} />
+            <Month monthNumber={10} monthData={dataOT[10]} year={year} />
+            <Month monthNumber={11} monthData={dataOT[11]} year={year} />
+            <Month monthNumber={12} monthData={dataOT[12]} year={year} />
           </div>
         </div>
         <div className="page right">
           <h2>{nameNT} - 2. polrok</h2>
           <div className="months">
-            <Month monthNumber={7} monthData={dataNZ[7]} year={year} />
-            <Month monthNumber={8} monthData={dataNZ[8]} year={year} />
-            <Month monthNumber={9} monthData={dataNZ[9]} year={year} />
-            <Month monthNumber={10} monthData={dataNZ[10]} year={year} />
-            <Month monthNumber={11} monthData={dataNZ[11]} year={year} />
-            <Month monthNumber={12} monthData={dataNZ[12]} year={year} />
+            <Month monthNumber={7} monthData={dataNT[7]} year={year} />
+            <Month monthNumber={8} monthData={dataNT[8]} year={year} />
+            <Month monthNumber={9} monthData={dataNT[9]} year={year} />
+            <Month monthNumber={10} monthData={dataNT[10]} year={year} />
+            <Month monthNumber={11} monthData={dataNT[11]} year={year} />
+            <Month monthNumber={12} monthData={dataNT[12]} year={year} />
           </div>
         </div>
       </div>
